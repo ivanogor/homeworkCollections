d6165a3f242d779114a2e1cd38197fd2deb16d10
ivanogor <ivanogor23@gmail.com>
2024-02-26 13:03:05 +0800

    Add argument check with StringUtil Library.

--- src/main/java/pro/sky/employeeBook/Service/EmployeeService.java
+++ src/main/java/pro/sky/employeeBook/Service/EmployeeService.java
@@ -1,5 +1,8 @@
 package pro.sky.employeeBook.Service;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 import pro.sky.employeeBook.model.Employee;
 import pro.sky.employeeBook.exceptions.EmployeeAlreadyAddedException;
 import pro.sky.employeeBook.exceptions.EmployeeNotFoundException;
@@ -16,6 +19,7 @@ public class EmployeeService {
     public Employee addEmployee(String firstName, String lastName, int department, double salary) {
         if (employeeBook.size() < MAX_NUMBER_OF_EMPLOYEE) {
             String name = firstName + lastName;
+            checkArguments(name);
             if (!employeeBook.containsKey(name)) {
                 Employee employee = new Employee(firstName, lastName, department, salary);
                 employeeBook.put(name, employee);
@@ -30,6 +34,7 @@ public class EmployeeService {
 
     public Employee findEmployee(String firstName, String lastName) {
         String name = firstName + lastName;
+        checkArguments(name);
         if (employeeBook.containsKey(name)) {
             return employeeBook.get(name);
         } else {
@@ -39,6 +44,7 @@ public class EmployeeService {
 
     public Employee removeEmployee(String firstName, String lastName) {
         String name = firstName + lastName;
+        checkArguments(name);
         Employee employee = employeeBook.get(name);
         if (employeeBook.remove(name, employee)) {
             return employee;
@@ -49,5 +55,12 @@ public class EmployeeService {
 
     public Collection<Employee> displayEmployees() {
         return Collections.unmodifiableCollection(employeeBook.values());
+    }
+
+    private void checkArguments(String name) {
+        boolean badOutcome = !StringUtils.isAlpha(name);
+        if (badOutcome) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
     }
 }
